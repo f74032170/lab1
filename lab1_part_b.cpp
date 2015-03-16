41a4bd87ef5a96d10914386cfaa0f197baf08308
f74032170 <s010554@fssh.khc.edu.tw>
2015-03-16 21:33:58 +0800

    this is a commit

---
+++ lab1_part_b.cpp
@@ -0,0 +1,23 @@
+
+
+#include<iostream>
+
+using namespace std;
+int main(){
+    int n;
+    cout<<"input n"<<endl;
+    cin>>n;
+    cout<<"the value n is "<<n<<endl;
+    while(n!=1){
+                if((n%2)==1){
+                             n=3*n+1;
+                             cout<<"the value n is "<<n<<endl;
+                             }
+                             else{
+                                  n=n/2;
+                             cout<<"the value n is "<<n<<endl;
+                             }
+                             }
+                            
+                             }
+
